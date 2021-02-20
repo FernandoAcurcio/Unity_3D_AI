10cd45ccbec00c0ef691592e0fdab026de270ffb
FernandoAcurcio <fernandoacurcio@hotmail.com>
2021-02-20 01:47:37 +0000

    - Added assets - Characters go to the goal point

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public UnityEngine.Vector3 Goal = new UnityEngine.Vector3(1,0,1);
+    public float Speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// using lateupdate so the movement only appens after everyting else
+    /// </summary>
+    void LateUpdate()
+    {
+        // multiply by deltatime to smoths the character movement
+        this.transform.Translate(Goal.normalized * Speed * Time.deltaTime);   
+    }
+}
---
+++ Assets/Scripts/MoveToGoal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    public float Accuracy = 0.01f;
+    public Color color = Color.red;
+    public Transform Goal;
+
+    void Start()
+    {
+        //this make our character to look directly to our player obj
+        this.transform.LookAt(Goal.position);       
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // calculate the direction vector between our character and the player obj
+        Vector3 direction = Goal.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, new Vector3(direction.x, 0.0f, direction.z), Color.blue);
+
+        if(direction.magnitude >= Accuracy)
+        {
+            Vector3 dir = new Vector3(direction.normalized.x, 0.0f, direction.normalized.z);
+            this.transform.Translate(dir * Speed * Time.deltaTime, Space.World);
+
+        }
+        
+    }
+}
